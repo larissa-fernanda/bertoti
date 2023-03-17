431231beb3321b24c21c9fde56999abbe059efa6
larissa-fernanda <laliffr@gmail.com>
2023-03-17 19:52:50 -0300

    add LojaBolos

---
+++ Engenharia_de_software/LojaBolos/src/Bolo.java
@@ -0,0 +1,28 @@
+public class Bolo {
+    private int id;
+    private int lote;
+    private Sabor sabor;
+    private double peso;
+    private double preco;
+
+    public int getId() {
+		return id;
+	}
+    public int getLote() {
+		return lote;
+	}
+    public Sabor getSabor() {
+		return sabor;
+	}
+    public double getPeso() {
+		return peso;
+	}
+    public double getPreco() {
+		return preco;
+	}
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Engenharia_de_software/LojaBolos/src/Comprador.java
@@ -0,0 +1,20 @@
+public class Comprador {
+    private String cpf;
+    private String nome;
+    private int telefone;
+    private Endereco endereco;
+
+    public String getCpf() {
+		return cpf;
+	}
+    public String getNome() {
+		return nome;
+	}
+    public int getTelefone() {
+		return telefone;
+	}
+    public Endereco getEndereco() {
+		return endereco;
+	}
+
+}
