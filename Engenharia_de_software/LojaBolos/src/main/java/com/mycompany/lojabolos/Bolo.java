4ae9908ca34f1639218f8b7af5bb0fb2929e08a3
Larissa Fernanda <111530654+larissa-fernanda@users.noreply.github.com>
2023-04-29 13:05:13 -0300

    Tirei os comentários do netbeans

--- Engenharia_de_software/LojaBolos/src/main/java/com/mycompany/lojabolos/Bolo.java
+++ Engenharia_de_software/LojaBolos/src/main/java/com/mycompany/lojabolos/Bolo.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.lojabolos;
 
-/**
- *
- * @author larissa
- */
 public class Bolo {
     private String sabor;
     private String tamanho;
