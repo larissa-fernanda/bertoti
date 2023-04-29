ae1705d66a0cb9ce1e9f76427a78eed2aac237d0
Larissa Fernanda <111530654+larissa-fernanda@users.noreply.github.com>
2023-04-29 13:05:58 -0300

    Tirei os comentários do netbeans

--- Engenharia_de_software/LojaBolos/src/main/java/com/mycompany/lojabolos/Cliente.java
+++ Engenharia_de_software/LojaBolos/src/main/java/com/mycompany/lojabolos/Cliente.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.lojabolos;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author larissa
- */
 public class Cliente {
     private String nome;
     private String endereco;
